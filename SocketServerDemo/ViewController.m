10b00bbcaf8a569559354ba3c959107f85e46424
Marshal <921871381@qq.com>
2021-05-21 11:20:20 +0800

    调整

--- SocketServerDemo/ViewController.m
+++ SocketServerDemo/ViewController.m
@@ -9,8 +9,6 @@
 #import <sys/socket.h>
 #import <netinet/in.h>
 #import <arpa/inet.h>
-#define SocketPort htons(8040)
-#define SocketIP   inet_addr("172.26.105.76")
 
 @interface ViewController ()
 
@@ -38,8 +36,10 @@
     //设置ip相关信息
     struct sockaddr_in socketAddr;
     socketAddr.sin_family   = AF_INET;
-    socketAddr.sin_port     = SocketPort;
-    socketAddr.sin_addr.s_addr     = SocketIP;
+    //htons : 将一个无符号短整型的主机数值转换为网络字节顺序，不同cpu 是不同的顺序 (big-endian大尾顺序 , little-endian小尾顺序)
+    socketAddr.sin_port = htons(8040);//设置端口号
+    //inet_addr是一个计算机函数，功能是将一个点分十进制的IPv4地址转换成一个长整数型数
+    socketAddr.sin_addr.s_addr =  inet_addr("172.26.105.76"); //设置ip
     //将前8个字节设置为0
     bzero(&(socketAddr.sin_zero), 8);
     
